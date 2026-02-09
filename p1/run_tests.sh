a298e56a5221488381b4769c40b138799ab63db8
George Chang <gchan22@stlawu.edu>
2026-02-09 07:33:23 +0000

    Describe your changes here

--- p1/run_tests.sh
+++ p1/run_tests.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
 #captures the program name
 program=$1
 
 #keeps track of the test number
 count=0
+# echo "$(find ./tests/outputs | grep "$count")" 
 #loops through the input to give a output when running program
-for i in $(find ./tests/inputs -type f | sort) 
+for i in ./tests/inputs/*
 do
     #checks the output given by input and compares with correct output
-    for j in $(find ./tests/outputs -type f | grep "$count" | sort)
-    do
-        if [ "$(./$program "$i")" = "$(cat "$j")" ]
-        then
-            echo "TEST "$count": PASSED"
-        else
-            echo "TEST "$count": FAILED"
-        fi
-    done
+   
+    if [ "$(./$program "$i")" = "$(cat "$(find ./tests/outputs | grep "$count")")" ]
+    then
+        echo "TEST "$count": PASSED"
+    else
+        echo "TEST "$count": FAILED"
+    fi
+    # echo "$(./$program "$i")"
+    # echo "$count"
+    echo "$(cat "$(find ./tests/outputs | grep "$count")")"
+
+    
     #increment current test number
     ((count++))
 done
