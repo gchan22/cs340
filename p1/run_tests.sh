8c020865fd41a68bd5e0fd6c6fd205837886086b
George Chang <gchan22@stlawu.edu>
2026-02-09 08:38:54 +0000

    Describe your changes here

--- p1/run_tests.sh
+++ p1/run_tests.sh
@@ -2,22 +2,21 @@
 #captures the program name
 program=$1
 
-#keeps track of the test number
-count=0
-# echo "$(find ./tests/outputs | grep "$count")" 
 #loops through the input to give a output when running program
 for i in ./tests/inputs/*
 do
+    #strips to just the name without path or suffix
+    testNa=$(basename "$i" .input)
+    #keeps track test number
+    testNum=${testNa#test_}
+    #finds output associated with the input
+    output="./tests/outputs/$testNa.output"
     #checks the output given by input and compares with correct output
-    
-    output=$(find ./tests/outputs | grep test_"$count".output)
     if [ "$(./$program "$i")" = "$(cat "$output")" ]
     then
-        echo "TEST "$count": PASS"
+        echo "TEST $testNum: PASS"
     else
-        echo "TEST "$count": FAIL"
+        echo "TEST $testNum: FAIL"
     fi
 
-    #increment current test number
-    ((count++))
 done
